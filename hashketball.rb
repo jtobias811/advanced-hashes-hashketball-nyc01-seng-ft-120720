ba1d6d31ef86c4e90bd204fbb22c6f613eded96a
Jacob Tobias <jtobias811@gmail.com>
2020-11-11 16:03:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -134,6 +134,7 @@ def num_points_scored(name)
     #binding.pry
     team_data[:players].each do |player_name, player_stats|
       if player_name == name
+        binding.pry
         return player_stats[:points]
         binding.pry
       elsif player_name =! name
